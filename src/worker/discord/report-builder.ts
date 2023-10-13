1fead9a15c09e42b8706cbcbc6106a995f524c92
Coalery <doralife12@gmail.com>
2023-10-14 00:30:03 +0900

    feat: 중첩 정렬 조건 구현

--- src/worker/discord/report-builder.ts
+++ src/worker/discord/report-builder.ts
@@ -1,3 +1,4 @@
+import { Task } from '../../task/task';
 import { PriorityOrder } from '../../task/priority';
 import { Summary } from '../../worker/summary-report';
 import { MessageBuilder } from './message-builder';
@@ -21,11 +22,9 @@ export class ReportBuilder {
     // 그룹에 할당된 Task 목록 빌드
     summary.groups.forEach(({ group, tasks }) => {
       result += `@${group.name}` + '\n';
-      tasks
-        .sort((a, b) => PriorityOrder[a.priority] - PriorityOrder[b.priority])
-        .forEach((task) => {
-          result += this.messageBuilder.build(task) + '\n';
-        });
+      tasks.sort(this.taskSortCompare).forEach((task) => {
+        result += this.messageBuilder.build(task) + '\n';
+      });
     });
 
     result += '\n';
@@ -33,15 +32,28 @@ export class ReportBuilder {
     // 멤버에게 할당된 Task 목록 빌드
     summary.members.forEach(({ member, tasks }) => {
       result += `@${member.name}` + '\n';
-      tasks
-        .sort((a, b) => PriorityOrder[a.priority] - PriorityOrder[b.priority])
-        .forEach((task) => {
-          result += this.messageBuilder.build(task) + '\n';
-        });
+      tasks.sort(this.taskSortCompare).forEach((task) => {
+        result += this.messageBuilder.build(task) + '\n';
+      });
     });
 
     result += '\n';
 
     return result;
+  }
+
+  private taskSortCompare(a: Task, b: Task): number {
+    // 1차 정렬 조건
+    const priorityDiff = PriorityOrder[a.priority] - PriorityOrder[b.priority];
+    if (priorityDiff !== 0) return priorityDiff;
+
+    // 2차 정렬 조건
+    if (!a.dueDate) return 1;
+    else if (!b.dueDate) return -1;
+    const dueDateDiff = a.dueDate.getTime() - b.dueDate.getTime();
+    if (dueDateDiff !== 0) return dueDateDiff;
+
+    // 3차 정렬 조건
+    return a.createdAt.getTime() - b.createdAt.getTime();
   }
 }
